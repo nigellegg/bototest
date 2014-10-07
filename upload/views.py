38f44935fd2c1f1132857a4927f25522e37a4c5b
nigel <nigel.legg@gmail.com>
2014-10-07 15:17:10 +0100

    views

--- upload/views.py
+++ upload/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext, Context, loader
 from django.contrib.auth.decorators import login_required
 from upload.forms import datafileForm
 from upload.models import datafile
+from django.core.files.storage import default_storage as s3_storage
 
 
 def getcsv(request):
@@ -12,7 +13,7 @@ def getcsv(request):
         form = datafileForm(request.POST, request.FILES)
         if form.is_valid():
             newdata = datafile()
-            newdata.csvx.save(form.cleaned_data['csvx'])
+            newdata.csvx.save(storage=s3_storage, form.cleaned_data['csvx'])
             newdata.csvname = form.cleaned_data['csvname']
             newdata.save()
             response_data = {}
