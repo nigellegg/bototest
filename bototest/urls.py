67ff7844d39dea1f7baa389b1aca5867bb800e61
nigel <nigel.legg@gmail.com>
2014-10-10 07:08:03 +0100

    urls

--- bototest/urls.py
+++ bototest/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bototest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
+    #url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
     url(r'^home/', TemplateView.as_view(template_name="home.html"), name="home"),
     #url(r'^upload/getcsv/$', 'upload.views.getcsv', name="getcsv"),
     url(r'^display/', include('display.urls'),
